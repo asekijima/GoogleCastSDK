f26f999093b26e4063d1e7ccf61061e52748f667
Arturo Sekijima <arturo.sekijima@hypergiant.com>
2023-06-15 16:57:29 -0300

    Switch checksum

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         .binaryTarget(
             name: "GoogleCastSDK",
             url: "https://github.com/asekijima/GoogleCastSDK/blob/main/GoogleCast.xcframework.zip",
-            checksum: "47289b356e318bc20b841f4adf34bd490e2ca8d62dcb231ac99a610ac4be9513"
+//            checksum: "47289b356e318bc20b841f4adf34bd490e2ca8d62dcb231ac99a610ac4be9513"
+            checksum: "422e11451ca8bfc028d2c1309add4d1cabbc5c03d780e797dee3659f9fbff43e"
         ),
         .target(
             name: "GoogleCast",
