de76c1166d66bffe31a10ecada7edf9acd8b6ccb
asekijima <asekijima@users.noreply.github.com>
2024-04-30 16:03:42 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "GoogleCast",
+            name: "GoogleCastSDK",
             targets: ["GoogleCast"]),
     ],
     targets: [
